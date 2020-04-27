81c39f8eb1fe99f68bbcaaebefc3572e7f4f8309
VictoryTheDragon <ka_ivj@live.com>
2020-04-27 13:45:44 -0500

    Temperatura

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ page.listen(pagePort, data => {
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('/dev/ttyUSB0');
 const parser = port.pipe(new Readline({delimiter: '\r\n'}));
-
+/*
 parser.on('data',temp => {
     console.log(temp);
 })
+*/
 
 wss.on('connection', ws => {
     parser.on('data', temp => {

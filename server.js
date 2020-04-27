996197b66b71150fced15f26df7b44bd452c377f
VictoryTheDragon <ka_ivj@live.com>
2020-04-27 17:37:35 -0500

    Ya lee y diferencia temperatura

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const Ws = require('ws');
 const express = require('express');
-const bodyParser = require('body-parser');
 const SerialPort = require('serialport');
+const mySql = require('mysql');
 
 const page = express();
 
 const wsPort = 5001;
 const pagePort = 5000;
+let string_ofice_temperature = "";
+let float_ofice_temperature = 0.0;
+let integer_alertas = 0;
+/*
+const base_de_datos = mySql.createConnection({
+    host: 
+})
+*/
 
 
 const wss = new Ws.Server({port: wsPort});
@@ -21,19 +29,33 @@ page.listen(pagePort, data => {
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('/dev/ttyUSB0');
 const parser = port.pipe(new Readline({delimiter: '\r\n'}));
-/*
-parser.on('data',temp => {
-    console.log(temp);
-})
-*/
+
 
 wss.on('connection', ws => {
+    console.log("Cliente conectado")
     parser.on('data', temp => {
-        console.log(temp);
+        for(let i = 15; i <= 18; i++){
+            string_ofice_temperature = string_ofice_temperature+temp[i];
+        }
+
         ws.send(temp);
+        float_ofice_temperature = parseFloat(string_ofice_temperature);
+        if(integer_alertas%60==0){
+            console.log("Alerta");
+        }
+        console.log(integer_alertas);
+        
+        string_ofice_temperature = "";
+        integer_alertas++;
+        
     })
-    console.log('Cliente conectado');
+    console.log('Cliente conectado'); //metodo para subscribir a un usuario
+    
 })
+
+
+
+
 
 
 

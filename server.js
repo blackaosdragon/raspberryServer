0f9d302add027c3f7bef0e59f1dd21381978ecfe
VictoryTheDragon <ka_ivj@live.com>
2020-04-28 13:14:40 -0500

    Leer datos

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const base_de_datos = mySql.createConnection({
 let tokens = [];
 
 asignar_tokens = (arreglo_de_tokens) => {
+    console.log(arreglo_de_tokens);
     for( let contador = 0; contador <= arreglo_de_tokens.length; contador++){
         tokens[contador] = arreglo_de_tokens[contador].token;
     }

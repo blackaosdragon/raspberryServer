e7f84e5384d74930204687025d6f7e1e691079ea
VictoryTheDragon <ka_ivj@live.com>
2020-04-29 17:43:32 -0500

    Activacion y desactivacion de alertas

--- server.js
+++ server.js
@@ -67,30 +67,30 @@ wss.on('connection', ws => {
             string_ofice_temperature = string_ofice_temperature+temp[i];
         }
         ws.send(temp);
+        console.log(`Alerta: ${alerta} `)
         float_ofice_temperature = parseFloat(string_ofice_temperature);
         if (float_ofice_temperature>24.9){
             integer_alertas++;
             alerta++;
-            console.log(`Alerta: ${alerta} `)
+            
             if(alerta==1){
                 console.log("Se activara alerta");
                 mensaje_push = setInterval(()=>{
-                    console.log("Alerta");
+                    console.log(`La temperatura tiene el valor de ${float_ofice_temperature}`);
                 },60000)
-            } else if (alerta == 0){
-                console.log("Se desactivara alerta");;
-                clearInterval(mensaje_push);
-            }
-            
+            }            
             if(integer_alertas%60==0){
                 integer_alertas = 0;
                 let envios = asignar_tokens();
                 
             }
         } else if (float_ofice_temperature<=24.9){
-            
             integer_alertas = 0;
             alerta=0;
+        }
+        if (alerta == 0){
+            console.log("Se desactivara alerta");;
+            clearInterval(mensaje_push);
         }
         console.log(integer_alertas);
         string_ofice_temperature = "";

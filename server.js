9f49ee6be30668b8d1f3e12431055c3772d1ba0f
VictoryTheDragon <ka_ivj@live.com>
2020-04-30 10:58:18 -0500

    Incluyendo firebase y cambiando textos de alerta y advertecia

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const Ws = require('ws');
 const express = require('express');
 const SerialPort = require('serialport');
 const mySql = require('mysql');
+const admin = require('firebase-admin');
+const serviceAccount = require("/home/ubuntu/home-8bea3-firebase-adminsdk-ilfkz-544a451f7b.json");
+const url_de_base_de_datos = 'https://home-8bea3.firebaseio.com/'
 
 const page = express();
 
 const wsPort = 5001;
 const pagePort = 5000;
+const intervalo_entre_alertas = 60000//en milisegundos
 let string_ofice_temperature = "";
 let float_ofice_temperature = 0.0;
 let integer_alertas = 0;
 let alerta = 1;
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: url_de_base_de_datos
+})
 
 const base_de_datos = mySql.createConnection({
     host: 'localhost',
@@ -21,14 +30,14 @@ const base_de_datos = mySql.createConnection({
 
 let tokens = [];
 
-activacion_de_alertas = (float_ofice_temperature,alerta) => {
+activacion_de_alertas = (float_ofice_temperature,alerta,texto) => {
     if (alerta == 1){
-        console.log("Se activara alerta de advertencia");
+        console.log(`Se activara notificaciones de ${texto}`);
         mensaje_push = setInterval(()=>{
             let envios = asignar_tokens();
             console.log(envios);
             console.log(`La temperatura tiene el valor de ${float_ofice_temperature}`);
-        },60000)        
+        },intervalo_entre_alertas) 
     }
 }
 
@@ -75,14 +84,7 @@ wss.on('connection', ws => {
         if (float_ofice_temperature > 24.9 && float_ofice_temperature <= 29.9){
             integer_alertas++;
             alerta++;
-            if(alerta==1){
-                console.log("Se activara alerta de advertencia");
-                mensaje_push = setInterval(()=>{
-                    let envios = asignar_tokens();
-                    console.log(envios);
-                    console.log(`La temperatura tiene el valor de ${float_ofice_temperature}`);
-                },60000)
-            }            
+                activacion_de_alertas(float_ofice_temperature,alerta,"advertencia");            
             if(integer_alertas%60==0){
                 integer_alertas = 0;
                 // let envios = asignar_tokens();
@@ -94,7 +96,7 @@ wss.on('connection', ws => {
         } else if ( float_ofice_temperature > 29.9 ){
             integer_alertas++;
             alerta++;
-            activacion_de_alertas(float_ofice_temperature,alerta);
+            activacion_de_alertas(float_ofice_temperature,alerta,"alerta");
         }
         if (alerta == 0){
             console.log("Se desactivara alerta");;

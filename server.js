fa94a63e9df48759594da70f7efd056a4b8bf9a4
VictoryTheDragon <ka_ivj@live.com>
2020-04-30 16:37:25 -0500

    Alerta de envio de notifis

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ let string_ofice_temperature = "";
 let float_ofice_temperature = 0.0;
 let integer_alertas = 0;
 let alerta = 1;
+let alertas_de_un_minuto = 150;
 
 
 
@@ -32,17 +33,17 @@ const base_de_datos = mySql.createConnection({
 
 let tokens = [];
 
-activacion_de_alertas = (float_ofice_temperature,alerta,texto) => {
+activacion_de_alertas = (float_ofice_temperature,alerta,texto,integer_alertas) => {
+    integer_alertas++;
     console.log(`Dentro de la funcion: ${float_ofice_temperature}`);
     if (alerta == 1){
         console.log(`Se activara notificaciones de ${texto}`);
-        setTimeout(()=>console.log(`Set time out: ${float_ofice_temperature}`),1000);
-        mensaje_push = setInterval(()=>{
+        if( integer_alertas % alertas_de_un_minuto == 0 ){
+            integer_alertas=0;
             let envios = asignar_tokens();
             console.log(envios);
             console.log(` ${texto}! La temperatura tiene el valor de ${float_ofice_temperature}`);
-            
-        },intervalo_entre_alertas) 
+        }
     }
 }
 
@@ -89,25 +90,23 @@ wss.on('connection', ws => {
         if (float_ofice_temperature > 24.9 && float_ofice_temperature <= 29.9){
             integer_alertas++;
             alerta++;
-            console.log(`Dentro de un if: ${float_ofice_temperature}`);
-            activacion_de_alertas(float_ofice_temperature,alerta,"advertencia"); 
-            float_ofice_temperature = 0.0;           
+            //console.log(`Dentro de un if: ${float_ofice_temperature}`);
+            activacion_de_alertas(float_ofice_temperature,alerta,"advertencia",integer_alertas); 
+            /*
             if(integer_alertas%150==0){
                 integer_alertas = 0;
-                console.log("1 minuto")
-                // let envios = asignar_tokens();
-                
-            }
+                console.log("1 minuto");
+                // let envios = asignar_tokens();  
+            }*/
         } else if (float_ofice_temperature<=24.9){
             integer_alertas = 0;
             alerta=0;
-            console.log(`Dentro de un if: ${float_ofice_temperature}`);
+            //console.log(`Dentro de un if: ${float_ofice_temperature}`);
         } else if ( float_ofice_temperature > 29.9 ){
             integer_alertas++;
             alerta++;
-            console.log(`Dentro del if: ${float_ofice_temperature}`);
-            activacion_de_alertas(float_ofice_temperature,alerta,"alerta");
-            float_ofice_temperature = 0.0;
+            //console.log(`Dentro del if: ${float_ofice_temperature}`);
+            activacion_de_alertas(float_ofice_temperature,alerta,"alerta",integer_alertas);
         }
         if (alerta == 0){
             console.log("Se desactivara alerta");;

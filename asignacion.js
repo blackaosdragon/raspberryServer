ab90edd4a61553c2a83585d0923dd1f7bcca0e3b
VictoryTheDragon <ka_ivj@live.com>
2020-05-05 13:16:38 -0500

    Preguntar si se peude transformar a float

--- asignacion.js
+++ asignacion.js
@@ -1,8 +1,13 @@
 module.exports = function string_to_float(data_reciber){
     let string_ofice_temperature = "";
+    let float_ofice_temperature = 0.0;
     for(let i = 15; i <= 18; i++){
         string_ofice_temperature = string_ofice_temperature+data_reciber[i];
     }
+    if(parseFloat(string_ofice_temperature)){
+        console.log("Se puede transformar a float");
+    }
+    //float_ofice_temperature = string_ofice_temperature
     //console.log(string_ofice_temperature)
     return string_ofice_temperature;
 }

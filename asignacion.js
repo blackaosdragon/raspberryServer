c6ae566cee60461570ce742f3c761c78f494a7ad
VictoryTheDragon <ka_ivj@live.com>
2020-05-20 18:00:17 -0500

    Cambios para el mensaje

--- asignacion.js
+++ asignacion.js
@@ -2,12 +2,18 @@ module.exports = function string_to_float(data_reciber){
     if (data_reciber==="No se ha recibido datos"){
 
     }
+    
     let string_ofice_temperature = "";
     let float_ofice_temperature = 0.0;
+
+    let mensaje_tamanio = data_reciber.length
+    console.log(mensaje_tamanio);
+
     for(let i = 15; i <= 18; i++){
         string_ofice_temperature = string_ofice_temperature+data_reciber[i];
     }
     //console.log(parseFloat(string_ofice_temperature))
+    if(string_ofice_temperature=="")
     if(parseFloat(string_ofice_temperature)){
         return float_ofice_temperature = parseFloat(string_ofice_temperature);
     } else {

d669accf6a87b69b74ca16b8a7c3937fd2b80929
VictoryTheDragon <ka_ivj@live.com>
2020-05-27 17:55:05 -0500

    INgreso correcto de todo el end point

--- home/funciones.js
+++ home/funciones.js
@@ -1,7 +1,10 @@
 let years_in_data_base = document.createElement('option');
 let id;
-fetch('192.168.0.101/consulta').then(response => {
-    console.log(response);
+let ip = '192.168.0.101';
+let puerto = 5001;
+let endpoint = '/consulta'
+fetch(`${ip}:${endpoint}${endpoint}`).then(response => {
+    //console.log(response);
     return response.json();
 }).then((data => {
     console.log(data);

e59c60cf2dddadf8b2b8deff6d68a74367154ff1
VictoryTheDragon <ka_ivj@live.com>
2020-06-19 18:01:42 -0500

    Arreglando puerto diicado

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ const httpServer = https.createServer({
    
    },page);
 
-httpServer.listen(pagePort,()=>{
-  console.log(`Servidor disponible en el puerto ${pagePort}`);
+httpServer.listen(puerto,()=>{
+  console.log(`Servidor disponible en el puerto ${puerto}`);
 })
 
 
@@ -58,11 +58,12 @@ page.use((req,res,next)=>{
 })
 page.use('/',express.static(__dirname+'/home'))
 
-
+/*
 const server = page.listen(pagePort, data => {
     //console.log(data);
     console.log(`Servidor corriendo en el puerto ${pagePort}`);
 });
+*/
 
 const Readline = SerialPort.parsers.Readline;
 let port = new SerialPort('/dev/ttyUSB0');

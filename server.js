e6d6390f9e105ecbd73af3ee90c267e3fc950d57
VictoryTheDragon <ka_ivj@live.com>
2020-06-19 17:58:59 -0500

    Puerto doble

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ lector.on('data', temp => {
     
 })
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(httpServer);
 
 const ioLector = port.pipe(new Readline({delimiter: '\r\n'}));
 

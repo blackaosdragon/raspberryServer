83a45e1940d07f808153c34c13ce822c34dd925d
VictoryTheDragon <ka_ivj@live.com>
2020-06-25 10:06:28 -0500

    Verificando los errores de consulta a la base de datos

--- querys.js
+++ querys.js
@@ -170,18 +170,19 @@ module.exports = {
         console.log(`Lugar: ${ubication} ${dia}/${mes}/${year}, Hora: ${hora_inicial}:${minuto_inicial} - ${hora_final}:${minuto_final}`)
         return new Promise( (resolve,reject) => {
             let elementos = [];
-            base_de_datos.query(`SELECT ubicacion,valor AS temperatura, fecha FROM monitoreo.Registro WHERE ubicacion='${ubication}' AND fecha>='${year}-${mes}-${dia} ${hora_inicial}:${minuto_inicial}:00' AND fecha<'${year}-${mes}-${dia} ${hora_final}:${minuto_final}:00;'`,(err,data,otro)=>{
+            base_de_datos.query(`SELECT ubicacion,valor AS temperatura, fecha FROM monitoreo.Registro WHERE ubicacion='${ubication}' AND fecha>='${year}-${mes}-${dia} ${hora_inicial}:${minuto_inicial}:00' AND fecha<'${year}-${mes}-${dia} ${hora_final}:${minuto_final}:00';`,(err,data,otro)=>{
                 if(err){
                     console.log(err);
                     reject(new Error());
                 } else {
-                    data.forEach((element,i) => {                      
+                    data.forEach((element,i) => {                    
                         elementos[i]={
                             ubicacion: element.ubicacion,
                             temperatura: element.temperatura,
                             fecha: element.fecha
                         }                       
                     });
+                    console.log(elementos);
                     resolve(elementos);
                 }
             })

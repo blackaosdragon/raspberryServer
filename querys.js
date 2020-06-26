70e00e0feba6525c1462e527219fe8e6e87a8f66
VictoryTheDragon <ka_ivj@live.com>
2020-06-26 10:16:12 -0500

    Ingresando datos correctos a la base de datos

--- querys.js
+++ querys.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     insertar_valores: (temperatura, lugar,ID) => {
         let tiempo = new Date();
-        base_de_datos.query(`INSERT INTO monitoreo.Bitacora(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID) VALUES ("${lugar}",${temperatura}, ${tiempo.getDay()},${tiempo.getMonth()},${tiempo.getFullYear()},${tiempo.getHours()},${tiempo.getMinutes},${tiempo.getSeconds()},${ID});`,(err,values,data)=>{
+        base_de_datos.query(`INSERT INTO monitoreo.Bitacora(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID) VALUES ("${lugar}",${temperatura}, ${tiempo.getDay()},${tiempo.getMonth()},${tiempo.getFullYear()},${tiempo.getHours()},${tiempo.getMinutes()},${tiempo.getSeconds()},${ID});`,(err,values,data)=>{
             if(err){
                 console.log(err);
             }
@@ -33,8 +33,8 @@ module.exports = {
             if(err){
                 console.log(err);
             }
-            console.log(datos);
-            console.log(campos);
+            //console.log(datos);
+            //console.log(campos);
         })
     },
     extraer_años: (ubicacion) => {

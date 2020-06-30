d24903d434ce6778b8ed6d8205afbafb4e347e6c
VictoryTheDragon <ka_ivj@live.com>
2020-06-30 16:33:36 -0500

    Ingresando el turno porque al salir el id 3 en undefined se buguea

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ const lector = port.pipe(new Readline({delimiter: '\r\n'}));
 //let parser = port.pipe(new Readline({delimiter: '\r\n'}));
 
 lector.on('data', temp => {
-    console.log(temp);
+    console.log(`${temp} turno: ${turno}`);
     let teempo = new Date();
     //let extra = "FALSE"
     //console.log(`Hora  de actualizacion: ${teempo.getHours()} : ${teempo.getMinutes()} : ${teempo.getSeconds()}`)

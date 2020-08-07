90802a8616fa8d0e4503828fd4491ca3a4085274
VictoryTheDragon <ka_ivj@live.com>
2020-08-07 15:45:47 -0500

    Teempo

--- senData.js
+++ senData.js
@@ -20,8 +20,8 @@ const sensores_en_total = 2;
 
 
 const fakedata = `ID: 1.0 temp: 18.3`;
-let reloj = new Date();
-let minuto_refresh = parseInt(reloj.getMinutes());
+let teempo = new Date();
+let minuto_refresh = parseInt(teempo.getMinutes());
 let temperatura = asignar.string_to_float(fakedata);
 let ubicacion = asignar.ubicar_dato(fakedata);
 let id = asignar.asignar_id(fakedata);

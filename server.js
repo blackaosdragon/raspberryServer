1bbe31d3ebf6a0ed2e7015f34d993a405917159a
VictoryTheDragon <ka_ivj@live.com>
2020-08-25 09:28:02 -0500

    Evitando textos extras

--- server.js
+++ server.js
@@ -363,7 +363,7 @@ page.post('/temperatura',(req,res)=>{
         io.emit('temp',`${req.body.id} ${req.body.temperatura}`);
         res.send(good);
     } else {
-        console.log(`La temperatura ${req.body.temperatura} para el id: ${req.body.id} no es valida`);
+        //console.log(`La temperatura ${req.body.temperatura} para el id: ${req.body.id} no es valida`);
     }
 })
 let response = {

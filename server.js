59fd9758d714d9f51fb34711e5dc16827d9b1112
VictoryTheDragon <ka_ivj@live.com>
2020-09-09 15:04:05 -0500

    ups x2

--- server.js
+++ server.js
@@ -513,7 +513,7 @@ setInterval(()=>cronometro(),1000);*/
     }
     setInterval(()=>temporizador(),1000);
     if(contador%120==0){
-        mensajes.notificacion_temperatura(req.body.temperatura,ubicacion);
+        mensajes.notificacion_temperatura(7.99,"Test");
     }
 //}
 page.post('/temperatura',(req,res)=>{

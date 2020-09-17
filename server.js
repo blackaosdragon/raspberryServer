2dcd41d75fa268844cb37b585ddda34c3eceacae
VictoryTheDragon <ka_ivj@live.com>
2020-09-17 17:43:18 -0500

    Intento de colocar bien la temperatura

--- server.js
+++ server.js
@@ -448,7 +448,7 @@ page.post('/temperatura',(req,res)=>{
             })
         }
         if(req.body.id==2){
-            let temp = req.body.temperatura - 2;
+            let temp = req.body.temperatura - 2.0;
             io.emit('temp',`${req.body.id} ${temp}`);
             //tokens.insertar_valores_2hour(temp,ubicacion,id);
             //tokens.insertar_valores(temp,ubicacion,id);

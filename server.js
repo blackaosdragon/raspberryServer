1ada1303a6d76e3dfd1f1ac7530e3bc7d730f8d8
VictoryTheDragon <ka_ivj@live.com>
2020-09-23 10:28:56 -0500

    COlocando limite de temperatura inferiror a el id 2

--- server.js
+++ server.js
@@ -324,11 +324,13 @@ page.post('/temperatura',(req,res)=>{
     
     //crono 1
     if( (parseFloat(req.body.temperatura)>temp_lim || parseFloat(req.body.temperatura)<temp_lim_inf) && parseInt(req.body.id)==1 && idContador==0){
+        
         idContador++;
         let tiempo = new Date();
         horas1Plasmado = parseInt(tiempo.getHours());
         minutos1Plasmado = parseInt(tiempo.getMinutes());
         temp1_irregular = true;
+        
     } else if( (parseFloat(req.body.temperatura)<=temp_lim && parseFloat(req.body.temperatura)>=temp_lim_inf) && parseInt(req.body.id)==1 && idContador>0){
         console.log(`Temperatura 1 normal se borra la hora: ${horas1Plasmado}:${minutos1Plasmado}`)
         horas1Plasmado=0;
@@ -351,13 +353,13 @@ page.post('/temperatura',(req,res)=>{
         
     }
     //crono 2
-    if((parseFloat(req.body.temperatura)-2)>temperatura_limite && parseInt(req.body.id)==2 && idContador2<=0){
+    if( ((parseFloat(req.body.temperatura)-2)>temperatura_limite || (parseFloat(req.body.temperatura)-2)<temp_lim_inf) && parseInt(req.body.id)==2 && idContador2<=0){
         idContador2++;
         let tiempo = new Date();
         horas2Plasmado = parseInt(tiempo.getHours());
         minutos2Plasmado = parseInt(tiempo.getMinutes());
         temp2_irregular = true;
-    } else if((parseFloat(req.body.temperatura)-2)<=temperatura_limite && parseInt(req.body.id)==2 && idContador2>0){
+    } else if( ( (parseFloat(req.body.temperatura)-2)<=temperatura_limite && (parseFloat(req.body.temperatura)-2)>temp_lim_inf) && parseInt(req.body.id)==2 && idContador2>0){
         console.log(`Temperatura 2 normal se borra la hora: ${horas2Plasmado}:${minutos2Plasmado}`)
         horas2Plasmado=0;
         minutos2Plasmado=0;

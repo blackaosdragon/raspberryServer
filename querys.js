f6c31d98b4e20c8a8198c751648d67ca429c776f
VictoryTheDragon <ka_ivj@live.com>
2020-09-25 10:17:40 -0500

    Falto ponber la variable de timepo

--- querys.js
+++ querys.js
@@ -440,8 +440,7 @@ module.exports = {
         
     },
     guardar_todos_los_datos: (temperatura, lugar,ID) => {
-        console.log("Buscando en emit");
-        let sentancia_1 = ``
+        let tiempo = new Date();
         return new Promise( (resolve,reject) => {
             base_de_datos.query(`INSERT INTO ${data_base}.${tabla_de_datos}(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID,Ubicacion) VALUES ("${lugar}",${temperatura}, ${tiempo.getDate()},${mes},${tiempo.getFullYear()},${tiempo.getHours()},${tiempo.getMinutes()},${tiempo.getSeconds()},${ID},'H. Cardiología S. XXI');`), (err,info,otro) =>{
                 console.log("Terminada la busqueda");

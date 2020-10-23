9819ebda4844f4a0c24eb5d38e43d48d67c02e55
VictoryTheDragon <ka_ivj@live.com>
2020-10-23 13:24:14 -0500

    Me equivoque al poner top en lugar de limit

--- querys.js
+++ querys.js
@@ -748,7 +748,7 @@ module.exports = {
         let hora = reloj.getHours();
         let hace_un_minuto = parseInt(reloj.getMinutes())-1;
         return new Promise( (res,rej) => {
-            base_de_datos.query(`SELECT Temperatura, Hora, Minuto FROM monitoreo.Bitacora WHERE id=${id} AND Hora=${hora} AND Minuto=${hace_un_minuto} ORDER BY turno DESC TOP 1;`
+            base_de_datos.query(`SELECT Temperatura, Hora, Minuto FROM monitoreo.Bitacora WHERE id=${id} AND Hora=${hora} AND Minuto=${hace_un_minuto} ORDER BY turno DESC LIMIT 1;`
             ,(err,data,otro)=>{
             if(err){
                 console.log(err);

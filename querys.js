d66831ad00a1e8f1c4260f3e06ef6ca907fe5d2f
VictoryTheDragon <ka_ivj@live.com>
2020-10-26 17:40:23 -0600

    Resolviendo parte del problema

--- querys.js
+++ querys.js
@@ -825,6 +825,12 @@ module.exports = {
         let tiempo = new Date();
         let mes = tiempo.getMonth() + 1;
         let minuto = tiempo.getMinutes() - 1;
+        let hora = parseInt(tiempo.getHours());
+        
+        if (parseInt(tiempo.getMinutes())==0){
+            minuto = 59;
+            hora = hora - 1;
+        }
         return new Promise((resolve,reject)=>{
             base_de_datos.query(`INSERT INTO ${data_base}.${tabla_de_datos}(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID,Ubicacion) VALUES ("${lugar}",${temp_aprox}, ${tiempo.getDate()},${mes},${tiempo.getFullYear()},${tiempo.getHours()},${minuto},60,${id},'H. Cardiología S. XXI');`
             , err => {

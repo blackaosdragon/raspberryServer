d4a93332aaf79dbf04747aba3c58720b69d29430
VictoryTheDragon <ka_ivj@live.com>
2020-10-26 11:00:52 -0600

    Camara inserto mal el minuit

--- querys.js
+++ querys.js
@@ -803,8 +803,9 @@ module.exports = {
         let lugar = "Cámara farmacia";
         let tiempo = new Date();
         let mes = tiempo.getMonth() + 1;
+        let minuto = tiempo.getMinutes() - 1;
         return new Promise((resolve,reject)=>{
-            base_de_datos.query(`INSERT INTO ${data_base}.${tabla_de_datos}(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID,Ubicacion) VALUES ("${lugar}",${temp_aprox}, ${tiempo.getDate()},${mes},${tiempo.getFullYear()},${tiempo.getHours()},${tiempo.getMinutes()},60,${id},'H. Cardiología S. XXI');`
+            base_de_datos.query(`INSERT INTO ${data_base}.${tabla_de_datos}(Lugar, Temperatura, Dia, Mes, Año, Hora, Minuto, Segundo,ID,Ubicacion) VALUES ("${lugar}",${temp_aprox}, ${tiempo.getDate()},${mes},${tiempo.getFullYear()},${tiempo.getHours()},${minuto},60,${id},'H. Cardiología S. XXI');`
             , err => {
                 if(err){
                     console.log(err);
